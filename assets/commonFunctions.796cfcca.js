95649c7086bc6d94312c095e547f1c712301d8d8
duc622 <duc.phan622@gmail.com>
2021-10-23 23:21:23 +0700

    Updates

--- assets/commonFunctions.796cfcca.js
+++
@@ -1,1 +0,0 @@
-const r=t=>t.split("T")[0];export{r as f};
--- assets/seed.63061cc4.js
+++
@@ -1,1 +0,0 @@
-var a=[{uid:"111111",name:"abc"},{uid:"22222",name:"aaaa"},{uid:"33333",name:"cccc"},{uid:"44444",name:"arrrbc"},{uid:"555",name:"arrrbc"},{uid:"44446664",name:"arrrbc"},{uid:"4333",name:"arrrbc"},{uid:"888",name:"arrrbc"}];export{a as s};
