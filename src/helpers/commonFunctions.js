5dff323f311d21b29a3c513ec9a7d7a794c796da
duc622 <duc.phan622@gmail.com>
2021-10-25 12:32:44 +0700

    update Post

--- src/helpers/commonFunctions.js
+++ src/helpers/commonFunctions.js
@@ -1,6 +1,7 @@
 export const formatDate = (date) => {
   return date.split("T")[0];
 };
+//-------------------------------------------------------------
 export const sortDescendingBasedOnFollowers = (arrayData) => {
   return arrayData.sort((a, b) => b.followers.length - a.followers.length);
 };
