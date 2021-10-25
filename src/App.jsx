a1227ddb37f5b02f119968652dafd27fa863f234
duc622 <duc.phan622@gmail.com>
2021-10-25 11:41:48 +0700

    done find user part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { lazy, Suspense, memo } from "react";
 import icon from "./assets/images/icon.svg";
 import ImageLoading from "./components/ImageLoading";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 //--------------------------------------------------
 document.querySelector("#icon").setAttribute("href", icon);
 //--------------------------------------------------
@@ -15,14 +15,14 @@ export default memo(function App() {
   //--------------------------------------------------
   return (
     <Suspense fallback={<ImageLoading />}>
-      <Router>
+      <BrowserRouter>
         <Header />
         <Switch>
           <Route exact path="/" component={DashBoard} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/find" component={Find} />
         </Switch>
-      </Router>
+      </BrowserRouter>
       <BackToTop />
     </Suspense>
   );
--- src/pages/Find/Users.jsx
+++ src/pages/Find/Users.jsx
@@ -1,15 +1,25 @@
 import React, { memo } from "react";
 import User from "./User";
-import findUsers from "../../seeds/findUsers.json"
+import findUsers from "../../seeds/findUsers.json";
+import getQueryFromURL from "../../helpers/useQuery";
 //--------------------------------------------------
 export default memo(function Users() {
+  const users = findUsers.users;
+  //--------------------------------------------------
+  const handleFilter = (val) => {
+    return val.userName.indexOf(getQueryFromURL("user_name")) !== -1
+      ? true
+      : false;
+  };
   //--------------------------------------------------
   return (
     <div className="container mt-4">
       <div className="row">
-         {findUsers.users.map((val) => (
-          <User key={val._id} userName={val.userName}/>
-        ))} 
+        {users
+          .filter((val) => handleFilter(val))
+          .map((val) => (
+            <User key={val._id} userName={val.userName} />
+          ))}
       </div>
     </div>
   );
