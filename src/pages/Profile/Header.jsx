8e97da524a34f8f105c8c937bf1e489045bb2236
duc622 <duc.phan622@gmail.com>
2021-10-26 01:59:20 +0700

    fix bug user_id of Profile

--- src/pages/Profile/Header.jsx
+++ src/pages/Profile/Header.jsx
@@ -17,7 +17,7 @@ export default memo(function Header({ userId }) {
       );
     };
     getData();
-  }, []);
+  }, [userId]);
   //--------------------------------------------------
   return (
     <div>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,21 +1,13 @@
-import React, { memo, useContext } from "react";
+import React, { memo } from "react";
 import Header from "./Header";
 import PostsProfile from "./PostsProfile";
-import getUserIdFromAccessToken from "../../helpers/getUserIdFromAccessToken";
 import getQueryFromURL from "../../helpers/getQueryFromURL";
-import { AuthContext } from "../../contexts/AuthProvider";
 //--------------------------------------------------
 export default memo(function Profile() {
-  const { stateAccessToken } = useContext(AuthContext);
   //--------------------------------------------------
   return (
     <div className="container">
-      <Header
-        userId={
-          getQueryFromURL("user_id") ||
-          getUserIdFromAccessToken(stateAccessToken)
-        }
-      />
+      <Header userId={getQueryFromURL("user_id")} />
       <PostsProfile />
     </div>
   );
