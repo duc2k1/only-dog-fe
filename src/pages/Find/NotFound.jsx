0ff0a53584f63be34626bd1638b6f1173c7ffe71
duc622 <duc.phan622@gmail.com>
2021-10-29 08:35:08 +0700

    resize image not found

--- src/pages/Find/NotFound.jsx
+++ src/pages/Find/NotFound.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function NotFound() {
   return (
     <img
-      className="text-center img-fluid"
+      className="text-center"
+      width="614"
+      height="302"
       src="https://indususedcars.com/assets/theme/images/no_result_found.png"
     />
   );
