4e8cfacf7aa3d19b527e46ab101220f2daafba72
duc622 <duc.phan622@gmail.com>
2021-11-06 22:57:42 +0700

    Updates

---
+++ assets/NotFound.374c7af4.js
@@ -0,0 +1,1 @@
+import{u as s}from"./vendor.a34827cf.js";import{j as r}from"./index.d9844d8d.js";function a(e){return new URLSearchParams(s().search).get(e)}function u(){return r("img",{className:"text-center",src:"https://indususedcars.com/assets/theme/images/no_result_found.png"})}export{u as N,a as g};
